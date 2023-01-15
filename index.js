1ac0bdf028916d6257fc5e83f3a85418a9fa5eda
James-Bennett-295 <74157600+James-Bennett-295@users.noreply.github.com>
2023-01-15 19:47:45 +0000

    misc

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
 const discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const config = require("./config.json");
 
 const client = new discord.Client();
 
 // bot ready
 client.once("ready", () => {
-    console.log("Ready!");
+	console.log("Ready!");
 
-    client.user.setPresence({
-        status: "dnd",
-        activity: {
-            type: "WATCHING",
-            name: "for ;;"
-        },
-    });
+	client.user.setPresence({
+		status: "dnd",
+		activity: {
+			type: "WATCHING",
+			name: "for ;;"
+		},
+	});
 });
 
 client.on("message", (message) => {
-    // primitive command handler
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-    const stdIMG = {format: "png", dynamic: true, size: 1024};
+	// primitive command handler
+	if (message.author.bot || message.webhookId) return;
+	if (!message.content.startsWith(config.prefix)) return;
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+	const avatarOptions = { format: "png", dynamic: true, size: 1024 };
 
 	// bolas
 	const taggedUser = message.mentions.users.first();
 
-    // command exec log
-    console.log(`Author: ${message.author.tag} ` +
-        `Date: ${message.createdAt}\nContent: ${message.content}`);
+	// command exec log
+	console.log(
+		`Author: ${message.author.tag}\n` +
+		`Date: ${message.createdAt}\n` +
+		`Content: ${message.content}`);
 
-    if (command === "ping") { // ping command
-        message.channel.send('your mother is full of portent juices');
+	if (command === "ping") { // ping command
+		message.channel.send('your mother is full of portent juices');
 
-    } else if (command === "args-info") { // args info command
-        if (args.length === 0)
-            return message.channel.send(`You didn't provide any arguments, ${message.author.username}`);
+	} else if (command === "args-info") { // args info command
+		if (args.length === 0)
+			return message.channel.send(`You didn't provide any arguments, ${message.author.username}`);
 
-        message.channel.send(`Your arguments were "${args}"`);
+		message.channel.send(`Your arguments were "${args.join(", ")}"`);
 
-    } else if (command === "avatar") {
-        const hasMention = message.mentions.users.size;
-        message.channel.send((hasMention ? taggedUser : 
-            message.author).displayAvatarURL(stdIMG));
-    }
+	} else if (command === "avatar") {
+		message.channel.send(
+			(taggedUser ? taggedUser : message.author)
+				.displayAvatarURL(avatarOptions));
+	}
 
-    // latinx blacklist
-    if (message.content.toLowerCase().includes("latinx"))
-        message.react("🖕");
+	// latinx blacklist
+	if (message.content.toLowerCase().includes("latinx"))
+		message.react("🖕");
 });
 
 
 // token
-client.login(token);
+client.login(config.token);
